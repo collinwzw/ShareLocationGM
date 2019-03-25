82141b986e9ba52128d2ebb9d148d77ae6f4dfee
Ziwen <zi.w.wang@mail.mcgill.com>
2019-03-25 19:44:29 -0400

    first commit

---
+++ ShareLocationGM/ShareLocationGM/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ShareLocationGM
+//
+//  Created by Ziwen Wang on 2019/3/25.
+//  Copyright © 2019年 Ziwen Wang. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        GMSServices.provideAPIKey("AIzaSyBHOmSSoLIOcF58-ZhGaCf6mSDmb63vKN4")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
